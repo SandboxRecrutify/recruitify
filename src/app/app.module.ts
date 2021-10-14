af8446edd80d83030d1caf0d92e0afb49148a41c
siarheichura <chura14@yandex.ru>
2021-10-14 18:23:01 +0300

    feat: add ng-zorro library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, NzCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
