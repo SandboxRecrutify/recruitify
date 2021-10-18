b29118a77c2dbae77c6ee8aa6e31786aa8338505
whatever-shaha <shahasadinov@gmail.com>
2021-10-18 17:01:13 +0500

    [https://trello.com/c/IXh82v5k/17-fe-make-login-page-and-its-styles] feat: made login page layout

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm() {
+    console.log(this.validateForm.value);
   }
-
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
 }
