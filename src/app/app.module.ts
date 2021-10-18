7a8635babae1707093bbdbaccf59a1f35ab70bb3
siarheichura <chura14@yandex.ru>
2021-10-18 16:56:13 +0300

    feat: add rate-stars component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NzCardModule, PagesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    NzCardModule,
+    PagesModule,
+    SharedModule,
+    FormsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+import { RatingStarsComponent } from './rating-stars/rating-stars.component';
+
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    SharedComponent
+    SharedComponent, RatingStarsComponent
   ],
   imports: [
     CommonModule,
-    SharedRoutingModule
-  ]
+    SharedRoutingModule,
+    NzRateModule,
+    NzIconModule,
+    FormsModule
+  ],
+  exports: [RatingStarsComponent]
 })
 export class SharedModule { }
