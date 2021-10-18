beb721475a99a38ad0de1153879bc9db9886d7cc
whatever-shaha <shahasadinov@gmail.com>
2021-10-18 11:35:35 +0500

    fix: refactoring on login-page componenet and created pages module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { NzCardModule } from 'ng-zorro-antd/card';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { GeneralModule } from './general/general.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +13,7 @@ import { GeneralModule } from './general/general.module';
     AppRoutingModule,
     CoreModule,
     NzCardModule,
-    GeneralModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
