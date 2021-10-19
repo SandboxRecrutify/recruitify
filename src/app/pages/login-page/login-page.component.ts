a7e1f678e4e2b2a99cd9b12c2100aaec71610fb1
whatever-shaha <shahasadinov@gmail.com>
2021-10-19 16:50:49 +0500

    feat: added validation for login-page form.

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -10,12 +10,20 @@ export class LoginPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   submitForm() {
-    console.log(this.validateForm.value);
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+    if (this.validateForm.valid) {
+      console.log(this.validateForm.value);
+    }
   }
   ngOnInit(): void {
     this.validateForm = this.fb.group({
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
     });
   }
 }
