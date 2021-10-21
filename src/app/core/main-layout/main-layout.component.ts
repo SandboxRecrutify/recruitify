40ea1d73b54ea4f3425e29edb22333386ead20c3
whatever-shaha <shahasadinov@gmail.com>
2021-10-21 15:38:34 +0500

    feat: made facade service

--- src/app/core/main-layout/main-layout.component.ts
+++ src/app/core/main-layout/main-layout.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FacadeService } from '../facade/facade.service';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
-
-  constructor() { }
+  constructor(private fc: FacadeService) {}
 
   ngOnInit(): void {
+    console.log(this.fc);
   }
-
 }
