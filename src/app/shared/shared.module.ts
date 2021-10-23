24d6ef7401ea678184b4b65e5edf8bccf8a3994f
krilian88 <inhuman_@mail.ru>
2021-10-23 11:53:48 +0300

    renamed sandboxes-page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,13 +10,15 @@ import { RatingStarsComponent } from './rating-stars/rating-stars.component';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzRateModule } from 'ng-zorro-antd/rate';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SandboxesPageComponent } from './pages/sandboxes-page/sandboxes-page.component';
-import { SandboxesListComponent } from './pages/sandboxes-page/sandboxes-list/sandboxes-list.component';
-import { SandboxComponent } from './pages/sandboxes-page/sandbox/sandbox.component';
+
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { ProjectsListComponent } from './pages/projects-page/projects-list/projects-list.component';
+import { ProjectComponent } from './pages/projects-page/project/project.component';
+
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
-import { ParticipantsComponent } from './pages/sandboxes-page/participants/participants.component';
+import { ParticipantsComponent } from './pages/projects-page/participants/participants.component';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -27,16 +29,16 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { ProjectsService } from './services/projects.service';
-import { ProjectsPageFacade } from './pages/sandboxes-page/sandboxes-page.facade';
+import { ProjectsPageFacade } from './pages/projects-page/projects-page.facade';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
     SharedComponent,
     RatingStarsComponent,
-    SandboxesPageComponent,
-    SandboxesListComponent,
-    SandboxComponent,
+    ProjectsPageComponent,
+    ProjectsListComponent,
+    ProjectComponent,
     ParticipantsComponent,
     CreateProjectComponent,
     ProfilePageComponent,
