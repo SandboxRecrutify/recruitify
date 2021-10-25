6325b699e24c32bafeb68025035f9c3e52040305
Siarhei Chura <78800419+siarheichura@users.noreply.github.com>
2021-10-25 08:01:43 +0300

    Add routing between candidates-page & profile-page  (#41)
    * feat: add routing between candidates-page & profile-page
    
    * update routing between canditates & profile pages

--- src/app/shared/models/Candidate.ts
+++ src/app/shared/models/Candidate.ts
@@ -1,9 +1,25 @@
 export interface Candidate {
-    id: number;
-    name: string;
-    location: string;
-    skill: string;
-    status: string;
-    feedback1: number;
-    feedback2: number;
+  id: number;
+  firstname: string;
+  lastname: string;
+  skill: string;
+  country: string;
+  city: string;
+  skype: string;
+  email: string;
+  phone: string;
+  englishLvl: string;
+  language: string;
+  testResult: string;
+  status: string;
+  projects: string;
+  feedback1: number;
+  feedback2: number;
+  applicationDate: string;
+  timeToContacts: string;
+  comments: string;
+  questions: string;
+  job: string;
+  certificates: string;
+  join: string;
 }
--- src/app/shared/pages/profile-page/profile-page.component.ts
+++ src/app/shared/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,9 @@
+import { filter, map } from 'rxjs/operators';
+import { ProfilePageFacade } from './profile-page.facade';
+import { paths } from './../../../app-routing.constants';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  candidate: any = {}
 
-  ngOnInit(): void {
+  constructor(private profilePageFacade: ProfilePageFacade, private toCandidatesPage: Router, private router: ActivatedRoute) {}
+
+  goToCandidatesList() {
+    this.toCandidatesPage.navigate([paths.candidates])
   }
 
+  ngOnInit(): void {
+    this.profilePageFacade.candidatesList$
+      .subscribe(responce => {
+        this.router.params.subscribe(params => {
+          this.candidate = responce.find(item => item.id === params.id)
+        })
+      })
+  }
 }
