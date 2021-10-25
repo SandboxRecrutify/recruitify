e502dfa4caebed3c7c9ddb6caf0344e928a91026
Siarhei Chura <78800419+siarheichura@users.noreply.github.com>
2021-10-25 14:28:14 +0300

    feat: add animation on profile-page appearance (#43)

--- src/app/shared/pages/profile-page/profile-page.component.ts
+++ src/app/shared/pages/profile-page/profile-page.component.ts
@@ -1,15 +1,26 @@
-import { filter, map } from 'rxjs/operators';
 import { ProfilePageFacade } from './profile-page.facade';
 import { paths } from './../../../app-routing.constants';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  styleUrls: ['./profile-page.component.scss'],
+  animations: [trigger('showPage', [
+    transition(':enter', [
+      style({transform: 'translateX(100%)'}),
+      animate('0.5s ease-in-out', style({transform: 'translate(0%'}))
+    ]),
+    transition(':leave', [
+      style({transform: 'translateX(0%)'}),
+      animate('0.5s ease-in-out', style({transform: 'translateX(-100%)'}))
+    ])
+  ])]
 })
+
 export class ProfilePageComponent implements OnInit {
 
   candidate: any = {}
