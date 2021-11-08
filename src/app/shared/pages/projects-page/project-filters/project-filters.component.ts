af443cdd504a4e108b882985d9abc4639f60dad0
whatever-shaha <shahasadinov@gmail.com>
2021-11-08 20:31:12 +0500

    made new layout for projects filter

---
+++ src/app/shared/pages/projects-page/project-filters/project-filters.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CreateProject } from 'src/app/shared/models/CreateProject';
+import { ProjectsService } from 'src/app/shared/services/projects.service';
+
+@Component({
+  selector: 'app-project-filters',
+  templateUrl: './project-filters.component.html',
+  styleUrls: ['./project-filters.component.scss'],
+})
+export class ProjectFiltersComponent implements OnInit {
+  @Input()
+  isVisible: boolean = false;
+
+  @Output()
+  onToggleIsVisible = new EventEmitter<boolean>();
+
+  data: CreateProject | undefined;
+  statuses = [
+    {
+      name: 'Active',
+      value: 'active',
+    },
+    {
+      name: 'Closed',
+      value: 'closed',
+    },
+    {
+      name: 'All',
+      value: 'all',
+    },
+  ];
+  statusInput: string = 'all';
+  primarySkillsInput: string[] = [];
+  dateSortInput: string = 'newest';
+
+  constructor(private projectsService: ProjectsService) {}
+  primarySkillsChange(values: string[]) {
+    this.primarySkillsInput = [...values];
+  }
+  onClose(): void {
+    this.onToggleIsVisible.emit(false);
+  }
+
+  onSubmit(): void {
+    console.log(this.statusInput, this.primarySkillsInput, this.dateSortInput);
+  }
+
+  ngOnInit(): void {
+    this.projectsService.getCreateProjectData().subscribe((data) => {
+      this.primarySkillsInput = data.primarySkills.map((skill) => skill.id!);
+      this.data = data;
+    });
+  }
+}
