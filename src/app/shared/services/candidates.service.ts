3c02dc818d80db7df2ecf3298f2cec735ac9c70e
whatever-shaha <shahasadinov@gmail.com>
2021-11-10 17:30:38 +0500

    connected profile page to backend endpoint

--- src/app/shared/services/candidates.service.ts
+++ src/app/shared/services/candidates.service.ts
@@ -5,25 +5,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 
-// const CANDIDATE_API = 'odata/Candidates';
+const CANDIDATE_API = '/odata/Candidates';
 @Injectable()
-// export class CandidatesService extends ApiService {
-//   constructor(private httpClient: HttpClient) {
-//     super(httpClient, CANDIDATE_API, CandidatesService.name);
-//   }
-
-//   getCandidates(): Observable<Candidate[]> {
-//     return super.get().pipe(map((d: any) => d.value));
-//   }
-// }
-export class CandidatesService {
-  constructor(private http: HttpClient) {}
+export class CandidatesService extends ApiService {
+  constructor(private httpClient: HttpClient) {
+    super(httpClient, CANDIDATE_API, CandidatesService.name, true);
+  }
 
   getCandidates(): Observable<Candidate[]> {
-    return this.http
-      .get(
-        'https://recruitifyexadel.herokuapp.com/odata/Candidates?$count=true'
-      )
-      .pipe(map((d: any) => d.value));
+    return super.get().pipe(map((d: any) => d.value));
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -71,6 +71,8 @@ import { TimeGridItemComponent } from './pages/calendar-page/time-grid-item/time
 import { TableForStaffComponent } from './pages/calendar-page/table-for-staff/table-for-staff.component';
 import { TimeGridRowComponent } from './pages/calendar-page/time-grid-row/time-grid-row.component';
 import { CandidatesGridComponent } from './pages/calendar-page/candidates-grid/candidates-grid.component';
+import { CandidateService } from './services/candidate.service';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 @NgModule({
   declarations: [
     SharedComponent,
@@ -101,6 +103,7 @@ import { CandidatesGridComponent } from './pages/calendar-page/candidates-grid/c
     CandidatesGridComponent,
   ],
   imports: [
+    NzBreadCrumbModule,
     CommonModule,
     SharedRoutingModule,
     FormsModule,
@@ -142,6 +145,7 @@ import { CandidatesGridComponent } from './pages/calendar-page/candidates-grid/c
     ProjectsService,
     ProjectsPageFacade,
     UserService,
+    CandidateService,
   ],
 })
 export class SharedModule {}
