70d0970a563bc04e5697ee125a229fafc33734ce
krilian88 <inhuman_@mail.ru>
2021-12-07 03:13:44 +0300

    fix: fixed display filter bug, switched to new endpoint

--- src/app/shared/models/CandidatesFilters.ts
+++ src/app/shared/models/CandidatesFilters.ts
@@ -1,20 +1,10 @@
 import { PrimarySkill } from './Project';
-import { EnglishLevel } from './EnglishLevel';
 import { CandidateLocation } from './CandidateLocation';
-import { Status } from './CandidatesStatus';
+
 
 export interface CandidatesFiltersValues {
  locations?: CandidateLocation[]
- date?: Date
- englishLevel?: (keyof typeof EnglishLevel)[];
- skill?: PrimarySkill[]
- statuses?: (keyof typeof Status)[];
- test?: number
- recruiter?: number
- entryInterview?: number
- mentor?: number
- finalInterview?: number
-
+ primarySkills?: PrimarySkill[]
 }
 
 
